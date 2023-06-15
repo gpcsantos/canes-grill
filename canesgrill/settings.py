4bbc799cfd23f6645122711486580f0a6d62ddcd
gpcsantos <gpcsantos@gmail.com>
2023-06-14 22:25:55 -0300

    inclusão dos arquivos estáticos, preparação e organização dos templates, conexão do banco de dados

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-61+@!ax0et=1t*coah*^by8bsudf+gt1n$ku31tngf$oyd4x7y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -74,10 +74,24 @@ WSGI_APPLICATION = 'canesgrill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_churras',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'options':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
@@ -118,7 +132,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / 'templates/static',
 ]
 
--- churras/views.py
+++ churras/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    pratos = {
+        1: 'Picanha',
+        2: 'Maminha',
+        3: 'Fraldinha'
+    }
+    
+    contexto = {
+        'lista_pratos' : pratos,
+    }
+    return render(request, 'index.html', contexto)
     
