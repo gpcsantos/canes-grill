c4315a474b45de3b2d123752edcc6f76793ab9c0
gpcsantos <gpcsantos@gmail.com>
2023-06-16 22:33:09 -0300

    atualizações aula 16-06-2023

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3306',
-        'options':{
+        'OPTIONS':{
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
--- churras/admin.py
+++ churras/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Prato
 # Register your models here.
+
+class ListandoPratos(admin.ModelAdmin):
+    list_display = [
+        'id','nome_prato','categoria', 'tempo_preparo','rendimento'
+    ]
+    list_display_links = [
+        'id','nome_prato',
+    ]
+    search_fields = ['nome_prato']
+    list_editable = ['categoria']
+
+
+admin.site.register(Prato, ListandoPratos)
