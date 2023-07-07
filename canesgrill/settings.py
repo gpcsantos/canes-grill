b6363f7830b1018cf5533dd65c04b970887009d0
gpcsantos <gpcsantos@gmail.com>
2023-07-07 20:17:10 -0300

    config settings STATICFILES

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static',
 ]
