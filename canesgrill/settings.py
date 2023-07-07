3cba62aad302aabfc2db23b5cde86cde431b2b94
gpcsantos <gpcsantos@gmail.com>
2023-07-07 20:41:19 -0300

    config settings STATICFILES new

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -150,11 +150,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static'),
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'templates/static'),
+# ]
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
